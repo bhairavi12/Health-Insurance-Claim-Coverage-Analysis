c584933cff9e5969aa8a09a918915e20d3f77227
bhairavi12 <67830643+bhairavi12@users.noreply.github.com>
2025-03-08 01:44:34 +0530

    Add files via upload

---
+++ Script.sql
@@ -0,0 +1,43 @@
+SELECT * FROM [dbo].[healthcare_dataset]
+
+
+Create view Heathcare As 
+SELECT * FROM [dbo].[healthcare_dataset]
+
+Select * from Heathcare
+
+SELECT INITCAP(Name) AS CapitalizedName
+FROM Heathcare;
+
+SELECT UPPER(LEFT(,1))+LOWER(SUBSTRING(word,2,LEN(word))) FROM [yourtable]
+
+
+------------------------------------------
+
+
+Select * from [dbo].[healthcare_dataset]
+
+
+--Total Approved Billing
+Select sum([Billing_Amount] )from healthcare_dataset
+where [Billing_Amount]<=45000
+
+
+--Total Rejected Billing
+Select sum([Billing_Amount] )from healthcare_dataset
+where [Billing_Amount]>=45000
+
+--Avg Claim amount
+Select avg([Billing_Amount] )from healthcare_dataset
+where [Billing_Amount]<=45000
+
+
+WITH ClaimsSummary AS (
+    SELECT 
+        COUNT(*) AS TotalClaims,
+        COUNT(CASE WHEN [Billing_Amount]<=45000 THEN 1 END) AS ApprovedClaims
+    FROM [dbo].[healthcare_dataset]
+)
+SELECT 
+    ApprovedClaims * 1.0 / TotalClaims AS ClaimApprovalRate
+FROM ClaimsSummary;
